0eeddc2d95a99378cd9dc553bfb734d23769a959
stalbot1979 <stalbot1979@gmail.com>
2013-07-10 17:50:08 -0400

    added method to grade a url

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util');
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -54,6 +56,14 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURL = function(url, status) {
+if (url instanceof Error) {
+    sys.puts('Error: ' + url.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    sys.puts(url);
+  }
+};
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -63,10 +73,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url', 'http://boiling-sierra-6739.herokuapp.com/')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    
+    if(program.url){
+        rest.get(program.url).on('complete', checkURL); 
+        }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
