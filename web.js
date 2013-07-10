d9e22ccc196016e0e30f941c56ac94f49167803f
Susan Talbot <susan.talbot@fotlinc.com>
2013-07-10 14:57:05 -0400

    Changed to read message from a file

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+fs.readFileSync('index.html', String);
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('index.html', String));
 });
 
 var port = process.env.PORT || 5000;
